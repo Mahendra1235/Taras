79cff735dd50b52149a786166415ed5330e3eae1
Mahendra <mahendrathungha.99@gmail.com.com>
2025-08-13 19:45:51 +0530

    Initiated Backend features to receive email for contactus and products page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import Logo from '../assets/LOGO-TARAS.png';
+import Logo from '../assets/logo.png';
 
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
