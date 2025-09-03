97720591d682a4b2aa8cfa017b5e07336877ecbb
Mahendra <mahendrathungha.99@gmail.com.com>
2025-09-03 11:32:04 +0530

    Updated server.js with transporter

--- src/Backend/Server.js
+++ src/Backend/Server.js
@@ -10,13 +10,24 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,    
+//     pass: process.env.EMAIL_PASS,     
+//   },
+// });
+
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // TLS
   auth: {
-    user: process.env.EMAIL_USER,    
-    pass: process.env.EMAIL_PASS,     
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
+
 
 // Styled HTML template function
 function createHtmlContent(data) {
